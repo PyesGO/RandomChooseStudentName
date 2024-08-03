7759107aed4b99403f0e0d3f5a811fb781e24f18
yjtfx <yjtfxgo@gmail.com>
2024-08-03 21:54:54 +0800

    test

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 #!/home/yjtfx/Personal_Data/Python_Venv/Normal/bin/python
 from main_ui import Application
 
-app = Application("抽号数")
+app = Application("Test")
 app.load_config()
 app.bulid_gui()
 app.show()
